f48cc1889c56349572059f13a95d780c7ecd8361
Benjamin Wiegand <126627496+Benjamin-Wiegand@users.noreply.github.com>
2025-06-19 16:23:57 -0700

    allow rotating just the drawable
    rotating the entire button becomes problematic. for example, if the button isn't square or has a rotation not divisible by 90, you will be able to tell that the background get rotated too.

--- app/src/main/java/io/benwiegand/atvremote/phone/ui/view/RemoteButton.java
+++ app/src/main/java/io/benwiegand/atvremote/phone/ui/view/RemoteButton.java
@@ -10,31 +10,70 @@ import static android.view.MotionEvent.ACTION_UP;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RotateDrawable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageButton;
 
+import io.benwiegand.atvremote.phone.R;
 import io.benwiegand.atvremote.phone.stuff.SerialInt;
 
 public class RemoteButton extends AppCompatImageButton {
 
     private int repeatInterval = -1;
     private int repeatDelay = 1000;
+    private float drawableRotation = 0;
     private final SerialInt holdSerial = new SerialInt();
     private boolean holdTriggered = false;
     private boolean clickCancelled = false;
 
     public RemoteButton(Context context) {
-        super(context, null);
+        super(context);
+        init(null);
     }
 
     public RemoteButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(attrs);
     }
 
     public RemoteButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        if (attrs == null) return;
+
+        for (int i = 0; i < attrs.getAttributeCount(); i++) {
+            if (attrs.getAttributeNameResource(i) != R.attr.rotateDrawable) continue;
+
+            drawableRotation = attrs.getAttributeFloatValue(i, 0);
+            if (drawableRotation == 0) break;
+            setImageDrawableInternal(rotateDrawable(getDrawable()));
+            break;
+        }
+    }
+
+    private Drawable rotateDrawable(Drawable drawable) {
+        if (drawableRotation == 0 || drawable == null) return drawable;
+        RotateDrawable rotateDrawable = new RotateDrawable();
+        rotateDrawable.setDrawable(drawable);
+        rotateDrawable.setToDegrees(drawableRotation);
+        rotateDrawable.setLevel(10000); // MAX_LEVEL (100% of lerp in RotateDrawable.onLevelChange())
+        return rotateDrawable;
+    }
+
+    private void setImageDrawableInternal(@Nullable Drawable drawable) {
+        super.setImageDrawable(drawable);
+    }
+
+    @Override
+    public void setImageDrawable(@Nullable Drawable drawable) {
+       setImageDrawableInternal(rotateDrawable(drawable));
     }
 
     public void setRepeat(int repeatDelay, int repeatInterval) {
