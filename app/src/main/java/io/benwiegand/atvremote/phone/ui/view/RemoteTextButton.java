b213d918a2eab554345401081d61bee76e79ab16
Benjamin Wiegand <126627496+Benjamin-Wiegand@users.noreply.github.com>
2025-06-23 18:17:37 -0700

    fix button text teleportation
    it can still happen if you tap it and quickly open the keyboard, but this is good enough for now

--- app/src/main/java/io/benwiegand/atvremote/phone/ui/view/RemoteTextButton.java
+++ app/src/main/java/io/benwiegand/atvremote/phone/ui/view/RemoteTextButton.java
@@ -87,6 +87,13 @@ public class RemoteTextButton extends AppCompatButton implements RemoteButton {
     @SuppressLint("ClickableViewAccessibility") // it does call performClick() just not directly
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+
+        // the button text disappears into the 9th dimension when some soft keyboards (including
+        // the aosp one) are visible, the button is tapped, and it has a ripple animation background
+        // (the material button ripple counts too). calling requestLayout() un-breaks it.
+        // I don't know why it disappears. but if you know then please let me know, I would like to know.
+        requestLayout();
+
         return buttonHandler.onTouchEvent(event);
     }
 
